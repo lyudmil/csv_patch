fc1a96fe521beea0b9cc87935ce7b2901f53b4a0
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2015-11-02 15:16:20 +0200

    Maintain the column schema across change batches

--- lib/csv_patch.rb
+++ lib/csv_patch.rb
@@ -6,17 +6,31 @@ module CsvPatch
   DEFAULT_BATCH_SIZE = 500
 
   def self.patch options
-    batches(options).each { |changes| apply_patch(options, changes) }
+    result_of_last_patch = nil
+    batches(options).each do |changes, last_batch|
+      input = result_of_last_patch || options[:input]
+      result_of_last_patch.close if result_of_last_patch
+      output = target_for_next_patch options[:output], last_batch
+      result_of_last_patch = apply_patch(input, output, changes)
+    end
   end
 
   private
+
+  def self.target_for_next_patch final_target, last_batch
+    return final_target if last_batch
+    Tempfile.new('csv_patch')
+  end
 
   def self.batches options
     StreamBatch.new(options[:changes], options[:batch_size] || DEFAULT_BATCH_SIZE)
   end
 
-  def self.apply_patch options, changes
-    Patch.new(input: options[:input], output: options[:output], changes: changes).apply
+  def self.apply_patch input, output, changes
+    Patch.new(input: input, output: output, changes: changes).apply
+
+    output.rewind
+    output
   end
 
 end
